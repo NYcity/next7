f186aa2f238444fcd0546e8a741b0f043339e004
Hongki Choi <mossei75@gmail.com>
2023-08-24 18:04:51 +0800

    2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,8 @@ export default function About() {
         Back to <Link href="/">Home</Link>
         <img
           src="https://s.yimg.com/ny/api/res/1.2/yFEwmcMtNEpX3g4YUjUmWQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTcwNTtoPTQ2Nw--/https://media.zenfs.com/en/afp.com.sg/af7eb5fd049a6e5a7774f17350f510cf"
-
+          width={200}
+          height={200}
           alt="Picture of the author"
         />
       </div>
